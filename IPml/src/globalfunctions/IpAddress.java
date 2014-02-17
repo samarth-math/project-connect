42a297c23315842eff37ea93c973a1b841d07422
Sam <samarth.math@gmail.com>
2014-02-18 01:12:37 +0530

    Ip address manipulating functions in global functions

--- IPml/src/globalfunctions/IpAddress.java
+++ IPml/src/globalfunctions/IpAddress.java
@@ -1,17 +1,26 @@
 package globalfunctions;
 import java.net.*;
-
+import org.apache.commons.net.util.*; // Depends on apache commons-net-3.3 library
 public class IpAddress 
 {
-	public static long ipToLong(InetAddress ip) 
+	public static long ipToLong(InetAddress ip) //Converts IP address to a long integer in order to make comparisons
 	{
         byte[] octets = ip.getAddress();
         long result = 0;
-        for (byte octet : octets) 
+        for (byte octet : octets) // for a variable octet defined by each element in the array octets
         {
-            result <<= 8;
-            result |= octet & 0xff;
+            result <<= 8;//left shift by 8 (due to 8 bytes)
+            result |= octet & 0xff;// Or the byte with the octet anded with the number 1 (0xff is hexadecimal for 1)
         }
         return result;
+	}
+	
+	
+ //***checks if the specified ip address falls in the specified network address
+	
+	public static boolean iprangeverify(String ipadd, String netadd)// Format of netadd : xxx.xxx.xxx.xxx/xx where /xx is network mask
+	{
+		SubnetUtils utils = new SubnetUtils(netadd);
+		return utils.getInfo().isInRange(ipadd);	
 	}
 }
