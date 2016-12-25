9108234c9a19119bc2fa1951de8079823ca926f2
Rishabh Baid <rishabh@bluestacks.com>
2016-12-26 01:05:11 +0530

    Correcting indentation and code clean up.

--- IPml/src/fileSending/Server.java
+++ IPml/src/fileSending/Server.java
@@ -14,8 +14,8 @@ import serverclient.MainStart;
 import GuiElements.FileTransferPanel;
 
 
-public class Server implements Runnable{
-	
+public class Server implements Runnable {
+
 	private ServerSocket serverSocket;
 	private String[] rootPath;
 	private String fileSeparator;
@@ -24,6 +24,7 @@ public class Server implements Runnable{
 	private float max;
 	private FileTransferPanel ftp;
 	public boolean stop = false;
+
 	public Server(int portNumber, FileTransferPanel ftp,String SaveAsPath) {
 		if(MainStart.ftpsocket==null)
 		{
@@ -37,27 +38,25 @@ public class Server implements Runnable{
 		this.ftp = ftp;
 		this.SaveAsPath = SaveAsPath;
 	}
+	
 	public void run() {
-		
-		try{
+
+		try {
 			Socket socket1 = null;
 			socket1 = serverSocket.accept();
 			receiveFile(socket1,SaveAsPath,ftp);
 			socket1.close();
-						
-			//serverSocket.close();
-			//MainStart.ftpsocket=null;
-		}
-		
-		catch (Exception e) {
+		} catch (Exception e) {
 			//Error
 		}
 	}
-public float geti(){
-	return i;
-}
-public void receiveFile(Socket socket,String SaveAsPath,FileTransferPanel ftp) throws IOException {
-	    
+
+	public float geti(){
+		return i;
+	}
+
+	public void receiveFile(Socket socket,String SaveAsPath,FileTransferPanel ftp) throws IOException {
+
 		/* File Header is of the format 
 		 * [f/d]*[file/directory Size]-[file/directory path][newline character]
 		 * f represents a file
@@ -66,15 +65,15 @@ public void receiveFile(Socket socket,String SaveAsPath,FileTransferPanel ftp) t
 		 * ASCII value of [newline character] is 10
 		 * ASCII value of [*] is 42
 		 * */
-	    int asterisk = 42;
-	    int minus = 45;
-	    int newline = 10;
-	    int endOfStream = -1;
+		int asterisk = 42;
+		int minus = 45;
+		int newline = 10;
+		int endOfStream = -1;
 		boolean entry = false;
 		boolean flag_directory = false;
 		String SaveAs = SaveAsPath.trim();
 		String fileSep = "//";
-		
+
 		for(int i=0;i<SaveAsPath.length();i++) {
 			if(SaveAsPath.charAt(i)=='/') {
 				fileSep = "/";
@@ -87,152 +86,153 @@ public void receiveFile(Socket socket,String SaveAsPath,FileTransferPanel ftp) t
 		}
 		int index = SaveAsPath.lastIndexOf(fileSep);
 		SaveAs = SaveAsPath.substring(0,index);
-		
-		
+
+
 		InputStream is = socket.getInputStream();
 		while(true) {
-			
-		long fileSize = 0;
-		long bytesRead=0;
-			
-		byte [] header = new byte[1];
-		String fileS = ""; //stores file size
-		String fileName = "";
-		String filePath = "";
-		
-		
-		// do while loop is used to extract the file header		
-		int current=0; // current represents number of byte read in each step
-		boolean flag = false; // flag will be set when '-' is encountered which is the separator in file header
 
-		char pathType = ' ';
-		do {
-			current = is.read(header);
-			if(header[0]==asterisk)
+			long fileSize = 0;
+			long bytesRead=0;
+
+			byte [] header = new byte[1];
+			String fileS = ""; //stores file size
+			String fileName = "";
+			String filePath = "";
+
+
+			// do while loop is used to extract the file header		
+			int current=0; // current represents number of byte read in each step
+			boolean flag = false; // flag will be set when '-' is encountered which is the separator in file header
+
+			char pathType = ' ';
+			do {
+				current = is.read(header);
+				if(header[0]==asterisk)
+					break;
+				pathType = (char)header[0];
+			} while(current!=endOfStream);
+
+			current=0;
+
+			do {
+				current = is.read(header);
+
+				if(flag==true)
+					filePath = filePath + (char)header[0]; 
+				else if(header[0]==minus && flag==false)
+					flag=true; // flag set when '-' encountered
+				else if(flag==false && header[0]!=newline)
+					fileS = fileS + (char) header[0];
+				bytesRead += current;
+
+			} while(header[0]!=newline && current!= endOfStream); // Loop terminates when newline character is read or end of file is reached
+
+			if(current==endOfStream)
 				break;
-			pathType = (char)header[0];
-		} while(current!=endOfStream);
-		
-		current=0;
-		
-		do {
-			current = is.read(header);
-		
-			if(flag==true)
-				filePath = filePath + (char)header[0]; 
-			else if(header[0]==minus && flag==false)
-				flag=true; // flag set when '-' encountered
-			else if(flag==false && header[0]!=newline)
-				fileS = fileS + (char) header[0];
-			bytesRead += current;
-			
-		} while(header[0]!=newline && current!= endOfStream); // Loop terminates when newline character is read or end of file is reached
-		
-		if(current==endOfStream)
-			break;
-		
-		for(int i=0;i<filePath.length();i++) {
-			if(filePath.charAt(i)=='\\') {
-				fileSeparator = "[\\\\]";
-				break;
+
+			for(int i=0;i<filePath.length();i++) {
+				if(filePath.charAt(i)=='\\') {
+					fileSeparator = "[\\\\]";
+					break;
+				}
+				if(filePath.charAt(i)=='/') {
+					fileSeparator = "[/]";
+					break;
+				}
 			}
-			if(filePath.charAt(i)=='/') {
-				fileSeparator = "[/]";
-				break;
-			}
-		}
-		if(entry==false) {
-			splitPath(filePath);
-			fileName = rootPath[rootPath.length-1];
-			entry=true;
-		}
-		else {
-			String f[] = filePath.split(fileSeparator);
-			fileName =  f[f.length-1];
-		}
-		fileName = fileName.trim();
-		
-		
-		fileS.trim();
-		fileSize = Long.parseLong(fileS);
-		if(!checkPath(filePath)) {
-			fileName = relativePath(filePath) ;
-		}
-		if(flag_directory == true) {
-			
-			SaveAsPath = SaveAs + fileName;
-		}
-		
-		File file = new File(SaveAsPath.trim()) ;
-		boolean status = false;
-		
-		if(pathType == 'd') { 
-			flag_directory = true;
-			status =  file.mkdir();
-			if(!status) {
-				JOptionPane.showMessageDialog(null,"Directory Creation failed for directory " + SaveAsPath,"The Three Musketeers say",JOptionPane.ERROR_MESSAGE);
-			}
-			continue;
-		}
-		else {
-			if(!file.exists()) 
-				file.createNewFile();
-		}
-	
-		FileOutputStream fos = new FileOutputStream(SaveAsPath.trim(),false);
-		BufferedOutputStream bos = new BufferedOutputStream(fos); 			
-		
-		// Contents of file is read and stored in bytearray which will then be written to file location
-		bytesRead=0;
-		int currentTotal = 0;
-		int chunkSize=256*1024; // chunkSize bytes are read in each step
-		long leftBytes = fileSize; // Number of bytes left to be written
-		
-		max = leftBytes;
-		
-		byte [] bytearray = new byte [chunkSize];
-		ftp.getprogbar().startProgress(max,this);
-		do {
-			if(chunkSize>leftBytes) {
-				bytesRead = is.read(bytearray,0,(int)leftBytes);
+			if(entry==false) {
+				splitPath(filePath);
+				fileName = rootPath[rootPath.length-1];
+				entry=true;
 			}
 			else {
-				bytesRead = is.read(bytearray,0,chunkSize);
+				String f[] = filePath.split(fileSeparator);
+				fileName =  f[f.length-1];
 			}
-			bos.write(bytearray,0,(int)bytesRead);
-			leftBytes = leftBytes - bytesRead;
-			i = i + bytesRead;			
-		} while(currentTotal<=fileSize && bytesRead > 0 && !stop);
-				
-		bos.flush();
-		bos.close();
-		
-		if(stop)
-		{
-			ftp.showMsg("File Transfer Cancelled");		
-			File f = new File(SaveAsPath);
-			deleteRecursive(f);// code to delete downloaded files so far.
-			break;
-		}
-		else
-		{
-			ftp.onCompleteUI();
+			fileName = fileName.trim();
+
+
+			fileS.trim();
+			fileSize = Long.parseLong(fileS);
+			if(!checkPath(filePath)) {
+				fileName = relativePath(filePath) ;
+			}
+			if(flag_directory == true) {
+
+				SaveAsPath = SaveAs + fileName;
+			}
+
+			File file = new File(SaveAsPath.trim()) ;
+			boolean status = false;
+
+			if(pathType == 'd') { 
+				flag_directory = true;
+				status =  file.mkdir();
+				if(!status) {
+					JOptionPane.showMessageDialog(null,"Directory Creation failed for directory " + SaveAsPath,"The Three Musketeers say",JOptionPane.ERROR_MESSAGE);
+				}
+				continue;
+			}
+			else {
+				if(!file.exists()) 
+					file.createNewFile();
+			}
+
+			FileOutputStream fos = new FileOutputStream(SaveAsPath.trim(),false);
+			BufferedOutputStream bos = new BufferedOutputStream(fos); 			
+
+			// Contents of file is read and stored in bytearray which will then be written to file location
+			bytesRead=0;
+			int currentTotal = 0;
+			int chunkSize=256*1024; // chunkSize bytes are read in each step
+			long leftBytes = fileSize; // Number of bytes left to be written
+
+			max = leftBytes;
+
+			byte [] bytearray = new byte [chunkSize];
+			ftp.getprogbar().startProgress(max,this);
+			do {
+				if(chunkSize>leftBytes) {
+					bytesRead = is.read(bytearray,0,(int)leftBytes);
+				}
+				else {
+					bytesRead = is.read(bytearray,0,chunkSize);
+				}
+				bos.write(bytearray,0,(int)bytesRead);
+				leftBytes = leftBytes - bytesRead;
+				i = i + bytesRead;			
+			} while(currentTotal<=fileSize && bytesRead > 0 && !stop);
+
+			bos.flush();
+			bos.close();
+
+			if(stop)
+			{
+				ftp.showMsg("File Transfer Cancelled");		
+				File f = new File(SaveAsPath);
+				deleteRecursive(f);// code to delete downloaded files so far.
+				break;
+			}
+			else
+			{
+				ftp.onCompleteUI();
+			}
 		}
 	}
-}
 
 	private void splitPath(String filePath) {
-		 rootPath = filePath.split(fileSeparator);
+		rootPath = filePath.split(fileSeparator);
 	}
-	
+
 	private boolean checkPath (String filePath) {
-			
+
 		String splitPath[] = filePath.split(fileSeparator);
 		if(rootPath.length==splitPath.length)
 			return true;
 		else
 			return false;
 	}
+	
 	private String relativePath(String filePath) {
 
 		String relativePath[] = filePath.split(fileSeparator);
@@ -242,19 +242,18 @@ public void receiveFile(Socket socket,String SaveAsPath,FileTransferPanel ftp) t
 			relativeLocation =  relativeLocation + File.separator + relativePath[index];
 			index = index + 1;
 		}
-			return relativeLocation;
+		return relativeLocation;
 	}
 
 	private boolean deleteRecursive(File path) {
-    if (path.isDirectory()) {
-        for (File file : path.listFiles()) {
-            if (!deleteRecursive(file))
-                return false;
-        }
-    }
-    return path.delete();
- }
-
+		if (path.isDirectory()) {
+			for (File file : path.listFiles()) {
+				if (!deleteRecursive(file))
+					return false;
+			}
+		}
+		return path.delete();
+	}
 }
-	
+
 
